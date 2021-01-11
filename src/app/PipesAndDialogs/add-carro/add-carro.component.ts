dac89379ab9498163756ec8efaa216394296555f
matias <mat.aguilerag94@gmail.com>
2021-01-11 17:50:12 -0300

    edits

--- src/app/PipesAndDialogs/add-carro/add-carro.component.ts
+++ src/app/PipesAndDialogs/add-carro/add-carro.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import {Item} from '../../Models/item.model'
 import { CarroService } from 'src/app/service/carro.service';
+import { LoginService } from 'src/app/service/login.service';
 @Component({
   selector: 'app-add-carro',
   templateUrl: './add-carro.component.html',
@@ -14,7 +15,8 @@ export class AddCarroComponent implements OnInit {
   constructor(
     @Inject(MAT_DIALOG_DATA) public producto:any,
     private dialogRef: MatDialogRef<AddCarroComponent>,
-    private _service:CarroService
+    private _service:CarroService,
+    private _login:LoginService
   ) { }
 
   ngOnInit(): void {
@@ -23,9 +25,11 @@ export class AddCarroComponent implements OnInit {
 
   agregarCarro(){
     this.item=new Item;
+    this.item.id = this._login.user();
     this.item.cantidad=this.cantidadProductos;
     this.item.producto=this.producto;
     this.item.producto.foto=null;
+    console.log(this.item);
     this._service.agregarACarro(this.item).subscribe((data:any)=>{
       console.log(data);
     });
